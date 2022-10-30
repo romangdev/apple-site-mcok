388a85dd343343c2b85d21ea2da5b950c2cfb42a
Roman Geathers <romangeathersdev@gmail.com>
2022-10-30 18:40:43 -0400

    Begin work on making navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-sm">
-      <div className="container">
+    <nav className="navbar navbar-expand-md">
+      <img src={require('./component-imgs/apple-logo.png')} className='nav-apple-logo' alt='apple logo'></img>
+      <div className="container nav-container">
         <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#n_bar" aria-controls="navbarNavAltMarkup" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
-        <img src={require('./component-imgs/apple-logo.png')} className='nav-apple-logo' alt='apple logo'></img>
         <div className="collapse navbar-collapse" id="n_bar">
             <ul className="navbar-nav">
                 <li className="nav-item active"><button className="nav-link">Store</button></li>
@@ -20,9 +20,9 @@ const Navbar = () => {
                 <li className="nav-item"><button className="nav-link">Support</button></li>
             </ul>
         </div>
-        <img src={require('./component-imgs/search-icon.png')} className='nav-search-icon' alt=''></img>
-        <img src={require('./component-imgs/shopping-icon.png')} className='nav-shopping-icon' alt=''></img>
       </div>
+      <img src={require('./component-imgs/search-icon.png')} className='nav-search-icon' alt=''></img>
+      <img src={require('./component-imgs/shopping-icon.png')} className='nav-shopping-icon' alt=''></img>
     </nav>
   )
 }
